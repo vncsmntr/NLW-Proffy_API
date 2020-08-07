c3a8303e10b22900561df8af7f6ae20d88c76d56
Vinicius Monteiro <vncsmntr.work@gmail.com>
2020-08-07 20:38:32 -0300

    NLW Proffy API

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+// GET    =>  Buscar Informação
+// POST   =>  Criar alguma informação nova
+// PUT    =>  Atualizar uma informação
+// DELETE =>  Deletar uma informação
+
+// Corpo  => Dados para criação ou atualização de dados (Request Body)
+// Route Params => Identificar o Recurso a atualizar
+// Query Params => usado para paginação, filtros ordenação
+
+// RUN KNEX FILE WITH "yarn knex migrate:latest --knexfile knexfile.ts"
+
+import express, { response } from 'express';
+import routes from './routes';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes); 
+
+
+
+app.listen(3333);
