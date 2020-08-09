1108702b08664a0849f526413330fac85b8aa9d9
Vinicius Monteiro <vncsmntr.work@gmail.com>
2020-08-09 13:32:51 -0300

    Configure Postgres

--- src/controllers/ClassesController.ts
+++ src/controllers/ClassesController.ts
@@ -23,17 +23,17 @@ export default class ClassesController {
     const timeInMinutes = convertHourToMinutes(time);
     //Listar aulas
     const classes = await db('classes')
-      .whereExists(function(){
-        this.select('class_schedule.*')
-          .from('class_schedule')
-          .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
-          .whereRaw('`class_schedule`.`week_day` = ?? ', [Number(week_day)])
-          .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
-          .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
-      })
-      .where('classes.subject', '=', subject)
-      .join('users', 'classes.user_id', '=', 'users.id')
-      .select(['classes.*', 'users.*']);
+    .whereExists(function() {
+      this.select('class_schedule.*')
+        .from('class_schedule')
+        .whereRaw('class_schedule.class_id = classes.id')
+        .whereRaw('class_schedule.week_day = ??', [Number(week_day)])
+        .whereRaw('class_schedule.from <= ??', [timeInMinutes])
+        .whereRaw('class_schedule.to > ??', [timeInMinutes])
+    })
+    .where('classes.subject', '=', subject)
+    .join('users', 'classes.user_id', '=', 'users.id')
+    .select(['classes.*', 'users.*'])
     return response.json(classes);
   }
 
@@ -56,7 +56,7 @@ export default class ClassesController {
         avatar,
         whatsapp,
         bio
-      });
+      }).returning('id');
     
       const user_id = insertedUsesIds[0];
     
@@ -64,7 +64,7 @@ export default class ClassesController {
         subject,
         cost,
         user_id
-      });
+      }).returning('id');
     
       const class_id = insertedClassesIds[0];
     
